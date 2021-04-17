3bcd9b563776c8bcb28e526c7d04765c57bcc7dd
Nate Marti <nate_marti@sil.org>
2021-04-17 17:03:18 +0100

    update gnome extensions; bump version to build

--- wasta-custom-car/resources/extensions/windowIsReady_Remover@nunofarruca@gmail.com/extension.js
+++ wasta-custom-car/resources/extensions/windowIsReady_Remover@nunofarruca@gmail.com/extension.js
@@ -1,34 +1,60 @@
+'use strict';
+
 const Main = imports.ui.main;
-const WindowAttentionHandler = imports.ui.windowAttentionHandler;
-const Shell = imports.gi.Shell;
-const Lang = imports.lang;
+const Handler = Main.windowAttentionHandler;
+const Gio = imports.gi.Gio;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
 
-function WindowIsReadyRemover() {
-    this._init();
-}
-
-WindowIsReadyRemover.prototype = {
-    _init : function() {
-        this._tracker = Shell.WindowTracker.get_default();
-        log('Disabling Window Is Ready Notification');
-        global.display.disconnect(Main.windowAttentionHandler._windowDemandsAttentionId);
-        global.display.disconnect(Main.windowAttentionHandler._windowMarkedUrgentId);
-    },
-
-    destroy: function () {
-        global.display.disconnect(this._handlerid);
+function getSettings() {
+    let GioSSS = Gio.SettingsSchemaSource;
+    let schemaSource = GioSSS.new_from_directory(
+        Me.dir.get_child("schemas").get_path(),
+        GioSSS.get_default(),
+        false
+    );
+    let schemaObj = schemaSource.lookup('org.gnome.shell.extensions.windowIsReady_Remover', true);
+    if (!schemaObj) {
+        throw new Error('cannot find schemas');
     }
+    return new Gio.Settings({ settings_schema: schemaObj });
 }
-
-let windowIsReadyRemover;
 
 function init() {
 }
 
 function enable() {
-    windowIsReadyRemover = new WindowIsReadyRemover();
+    if (Handler._windowDemandsAttentionId) {
+        global.display.disconnect(Handler._windowDemandsAttentionId);
+        Handler._windowDemandsAttentionId = null;
+    }
+    if (Handler._windowMarkedUrgentId) {
+        global.display.disconnect(Handler._windowMarkedUrgentId);
+        Handler._windowMarkedUrgentId = null;
+    }
 }
 
 function disable() {
-    windowIsReadyRemover.destroy();
+    let settings = getSettings();
+    let preventDisable = settings.get_boolean('prevent-disable');
+
+    if (preventDisable) {
+        //log(Me.metadata.name + ' > Disable was prevented');
+    } else {
+        if (!Handler._windowDemandsAttentionId) {
+            Handler._windowDemandsAttentionId = global.display.connect(
+                'window-demands-attention',
+                (display, window) => {
+                    Handler._onWindowDemandsAttention(display, window);
+                }
+            );
+        }
+        if (!Handler._windowMarkedUrgentId) {
+            Handler._windowMarkedUrgentId = global.display.connect(
+                'window-marked-urgent',
+                (display, window) => {
+                    Handler._onWindowDemandsAttention(display, window);
+                }
+            );
+        }
+    }
 }
