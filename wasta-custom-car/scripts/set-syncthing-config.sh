7e53e1f665bbffb77ff0bf7629f4dd96d15dbf69
Nate Marti <nate_marti@sil.org>
2021-04-17 15:30:51 +0100

    try dbus-launch with deb-systemd-invoke

--- wasta-custom-car/scripts/set-syncthing-config.sh
+++ wasta-custom-car/scripts/set-syncthing-config.sh
@@ -201,9 +201,4 @@ if [[ ! -e "$st_ignore" ]]; then
 fi
 
 # Ensure that syncthing is restarted after editing config.xml.
-# sudo --user=$REAL_USER --set-home dbus-launch systemctl --user restart syncthing.service
-sudo --user=$REAL_USER deb-systemd-invoke --user restart syncthing.service
-# if [[ $? -eq 0 ]]; then
-    # immediately kill new dbus process.
-    # kill $(pgrep -n dbus-daemon)
-#fi
+sudo --user=$REAL_USER --set-home dbus-launch deb-systemd-invoke --user restart syncthing.service
